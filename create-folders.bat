e19c2c87d2a93423f86cfa3d49d32307c2a19b96
Henoc N'GASAMA <ngasamah@gmail.com>
2025-06-13 21:09:58 +0000

    Srcipt ittératif de création de dossiers

---
+++ create-folders.bat
@@ -0,0 +1,13 @@
+@echo off
+cd src
+mkdir components
+mkdir components\Editor
+mkdir components\UI
+mkdir hooks
+mkdir utils
+mkdir types
+mkdir styles
+mkdir store
+mkdir pages
+echo Dossiers créés avec succès!
+pause
